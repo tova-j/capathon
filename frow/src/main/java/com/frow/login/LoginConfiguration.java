fcf1809448f97e7ddf1674b1d2563c0d398d786e
jycapgemin <159958292+jycapgemin@users.noreply.github.com>
2024-02-19 16:51:53 -0600

    created "dynamic" hard coded login

--- frow/src/main/java/com/frow/login/LoginConfiguration.java
+++ frow/src/main/java/com/frow/login/LoginConfiguration.java
@@ -1,9 +1,13 @@
 package com.frow.login;
 
+import java.util.function.Function;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
@@ -11,11 +15,28 @@ public class LoginConfiguration {
 	
 	@Bean
 	public InMemoryUserDetailsManager createUserDetailsManager() {
-		UserDetails userDetails = User.withDefaultPasswordEncoder()
-				.username("user")
-				.password("1234")
+		
+		UserDetails userDetails1 = createNewUser("user1", "1234");
+		UserDetails userDetails2 = createNewUser("user2", "123456");
+
+		return new InMemoryUserDetailsManager(userDetails1, userDetails2);
+	}
+
+	private UserDetails createNewUser(String userName, String password) {
+		Function<String, String> passwordEncoder
+			= input -> passwordEncoder().encode(input);
+			
+		UserDetails userDetails = User.builder()
+				.passwordEncoder(passwordEncoder)
+				.username(userName)
+				.password(password)
 				.roles("USER","ADMIN")
 				.build();
-		return new InMemoryUserDetailsManager(userDetails);
+		return userDetails;
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
