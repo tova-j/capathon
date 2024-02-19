c01c246e3a29260d1225b7e70e8c030c9bf7ce1a
Tova Just <tova.just@cagemini.com>
2024-02-19 14:17:21 -0600

    Adding Spring Boot base files

---
+++ frow/src/main/java/com/frow/FrowApplication.java
@@ -0,0 +1,13 @@
+package com.frow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrowApplication.class, args);
+	}
+
+}
---
+++ frow/src/test/java/com/frow/FrowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
