cb65aa6a0a1987df766faa5561dbf45272441598
Tova Just <tova.just@cagemini.com>
2024-02-19 17:09:47 -0600

    adding functionality for designer and vendor url endpoints using JSP

--- frow/src/main/java/com/frow/designer/DesginerWebAppController.java
+++ frow/src/main/java/com/frow/designer/DesginerWebAppController.java
@@ -1,8 +1,19 @@
 package com.frow.designer;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class DesginerWebAppController {
+    
+    @RequestMapping(value="designer", method=RequestMethod.GET)
+    public String gotoDesignerWelcomePage() {
+        return "designerWelcome";
+    }
+
+    
     
 }
