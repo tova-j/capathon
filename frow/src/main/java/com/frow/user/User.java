14c7a55a846f92c8ec3fe5a7fe806476e4f75aff
tova-j <75283289+tova-j@users.noreply.github.com>
2024-02-19 18:02:41 -0600

    Fleshing out User class with constructor, setters, getters, and toString methods

--- frow/src/main/java/com/frow/user/User.java
+++ frow/src/main/java/com/frow/user/User.java
@@ -4,5 +4,44 @@ public class User {
 	private int id;
 	private String firstName;
 	private String lastName;
+
+	public User(int id, String firstName, String lastName) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getFullName() {
+		return firstName + " " + lastName;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("User [id=%d, firstName=%s, lastName=%s", id, firstName, lastName);
+	}
 	
 }
