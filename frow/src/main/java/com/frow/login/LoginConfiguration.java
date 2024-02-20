b09a060445a2d636d6911298ea293044fac81dda
jycapgemin <159958292+jycapgemin@users.noreply.github.com>
2024-02-20 11:54:29 -0600

    added logout functionality

--- frow/src/main/java/com/frow/login/LoginConfiguration.java
+++ frow/src/main/java/com/frow/login/LoginConfiguration.java
@@ -40,4 +40,5 @@ public class LoginConfiguration {
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+	
 }
--- frow/src/main/java/com/frow/login/LoginController.java
+++ frow/src/main/java/com/frow/login/LoginController.java
@@ -2,11 +2,17 @@ package com.frow.login;
 
 import java.util.Collection;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class LoginController {
@@ -26,5 +32,16 @@ public class LoginController {
 
 		// if user does not have either role, they see just homepage
 		return "homepage";
+	}
+
+	// handle log out
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	public String logOut(HttpServletRequest request, HttpServletResponse response) {
+		// if user does not have either role, they see just homepage
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth != null) {    
+			new SecurityContextLogoutHandler().logout(request, response, auth);
+		}
+		return "redirect:";
 	}
 }
