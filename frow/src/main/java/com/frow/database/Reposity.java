cb56897ad7ba2dc42c047fd495b5aa50148e255a
tova-j <75283289+tova-j@users.noreply.github.com>
2024-02-21 09:06:13 -0600

    Created Repository file

---
+++ frow/src/main/java/com/frow/database/Reposity.java
@@ -0,0 +1,11 @@
+package com.frow.database;
+
+import org.springframework.stereotype.Repository;
+
+import jakarta.transaction.Transactional;
+
+@Repository
+@Transactional
+public class Reposity {
+    
+}
