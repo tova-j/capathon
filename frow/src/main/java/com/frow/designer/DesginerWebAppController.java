cecdb3d726196f21d939d076797692178e04a946
tova-j <75283289+tova-j@users.noreply.github.com>
2024-02-20 18:45:31 -0600

    adding endpoints and request mapping for contact-us and vendor sign up functionality

--- frow/src/main/java/com/frow/designer/DesginerWebAppController.java
+++ frow/src/main/java/com/frow/designer/DesginerWebAppController.java
@@ -3,6 +3,7 @@ package com.frow.designer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -13,6 +14,15 @@ public class DesginerWebAppController {
         return "designerWelcome";
     }
 
+    @RequestMapping(value="contact-us", method=RequestMethod.GET)
+    public String gotoContactUsPage() {
+        return "contactUs";
+    }
+
+    @RequestMapping(value="contact-us", method=RequestMethod.POST)
+    public String gotoContactUsConfirmationPage() {
+        return "contactUsConfirmation";
+    }
     
     
 }
