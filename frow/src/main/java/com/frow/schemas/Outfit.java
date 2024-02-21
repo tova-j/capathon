6aab29351e1bffaef5f7f8527a967f67f45aa1e2
tova-j <75283289+tova-j@users.noreply.github.com>
2024-02-20 21:27:03 -0600

    Update Outfit class to add an image URL, plus relevant getters and setters

--- frow/src/main/java/com/frow/schemas/Outfit.java
+++ frow/src/main/java/com/frow/schemas/Outfit.java
@@ -9,8 +9,9 @@ public class Outfit {
     private double price;
     private ArrayList<Piece> pieces;
     private int numPieces;
+    private String imageUrl;
 
-    public Outfit(int outfitId, int fashionLineId) {
+    public Outfit(int outfitId, int fashionLineId, String imageUrl) {
         this.id = outfitId;
         this.fashionLineId = fashionLineId;
 
@@ -30,6 +31,14 @@ public class Outfit {
 
     public int getId() {
         return id;
+    }
+
+    public int getFashionLineId() {
+        return fashionLineId;
+    }
+
+    public void setFashionLineId(int fashionLineId) {
+        this.fashionLineId = fashionLineId;
     }
 
     public String getPieces() {
@@ -52,8 +61,16 @@ public class Outfit {
         return price;
     }
 
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
     @Override
     public String toString() {
-        return String.format("Outfit [id=%d, fashionLine=%d, price=%f, numPieces=%d]", id, fashionLineId, price, numPieces);
+        return String.format("Outfit [id=%d, fashionLine=%d, price=%f, imageUrl=%s, numPieces=%d]", id, fashionLineId, price, imageUrl, numPieces);
     }
 }
