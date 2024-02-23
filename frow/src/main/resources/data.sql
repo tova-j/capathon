c2aea92065b94ba651b13ed950c48317e85ce170
tova-j <75283289+tova-j@users.noreply.github.com>
2024-02-23 00:02:45 -0600

    Formatting and adding new data for designers

--- frow/src/main/resources/data.sql
+++ frow/src/main/resources/data.sql
@@ -1,10 +1,9 @@
 insert into piece (piece_id, outfit_id, name, price, image_url, gender, clothing_type, size)
 values (1001, 5001, 'Jeans', 20.99, 'www.google.com', 0, 1, 'Medium');
 insert into piece (piece_id, outfit_id, name, price, image_url, gender, clothing_type, size)
-values (1002, 5001, 'White Shirt', 15.99, 'www.google.com', 2, 0, 'Small');
+values (1002, 5001, 'Polo Shirt', 15.99, 'www.google.com', 2, 0, 'Small');
 insert into piece (piece_id, outfit_id, name, price, image_url, gender, clothing_type, size)
 values (1003, 5002, 'Dress', 42.99, 'www.google.com', 1, 2, 'Large');
-
 insert into piece (piece_id, outfit_id, name, price, image_url, gender, clothing_type)
 values (1004, 5004, 'Pants', 200.99, 'www.google.com', 0, 1);
 insert into piece (piece_id, outfit_id, name, price, image_url, gender, clothing_type)
@@ -49,21 +48,40 @@ insert into fashion_line (fashion_line_id, designer_id, designer_name, num_outfi
 values (8009, 4, 'Collection Delta', 0, 899.99, 3);
 
 insert into custom_user (id, username, password, first_name, last_name, role)
-values (1, 'user1', '1234', 'Joonyoung', 'Im', 'ROLE_DESIGNER');
+values (1, 'user1', '1234', 'Coco', 'Chanel', 'ROLE_DESIGNER');
 insert into custom_user (id, username, password, first_name, last_name, role)
-values (2, 'user4', '1234', 'Tova  ', 'Just', 'ROLE_DESIGNER');
+values (2, 'user4', '1234', 'Giorgio', 'Armani', 'ROLE_DESIGNER');
 insert into custom_user (id, username, password, first_name, last_name, role)
-values (3, 'user3', '1234', 'Aditya', 'Pothanaboyina', 'ROLE_DESIGNER');
+values (3, 'user3', '1234', 'Ralph', 'Lauren', 'ROLE_DESIGNER');
 insert into custom_user (id, username, password, first_name, last_name, role)
-values (4, 'user1', '1234', 'Giorgio', 'Armani', 'ROLE_DESIGNER');
+values (4, 'user1', '1234', 'Valentino', 'Garavani', 'ROLE_DESIGNER');
 insert into custom_user (id, username, password, first_name, last_name, role)
-values (5, 'user4', '1234', 'Calvin', 'Klein', 'ROLE_DESIGNER');
+values (5, 'user4', '1234', 'Marc', 'Jacobs', 'ROLE_DESIGNER');
 insert into custom_user (id, username, password, first_name, last_name, role)
 values (6, 'user6', '1234', 'Michael', 'Kors', 'ROLE_DESIGNER');
 insert into custom_user (id, username, password, first_name, last_name, role)
-values (7, 'designer', '1234', 'John', 'Doe', 'ROLE_DESIGNER');
+values (9, 'user7', '1234', 'Yves', 'Saint Laurent', 'ROLE_DESIGNER');
 insert into custom_user (id, username, password, first_name, last_name, role)
-values (8, 'vendor', '1234', 'Emma', 'Smith', 'ROLE_VENDOR');
+values (10, 'user8', '1234', 'Christian', 'Dior', 'ROLE_DESIGNER');
+insert into custom_user (id, username, password, first_name, last_name, role)
+values (11, 'user9', '1234', 'Donatella', 'Versace', 'ROLE_DESIGNER');
+insert into custom_user (id, username, password, first_name, last_name, role)
+values (12, 'user10', '1234', 'Stella', 'McCartney', 'ROLE_DESIGNER');
+insert into custom_user (id, username, password, first_name, last_name, role)
+values (13, 'user11', '1234', 'Rihanna', 'Fenty', 'ROLE_DESIGNER');
+insert into custom_user (id, username, password, first_name, last_name, role)
+values (14, 'user12', '1234', 'Vera', 'Wang', 'ROLE_DESIGNER');
+insert into custom_user (id, username, password, first_name, last_name, role)
+values (15, 'user13', '1234', 'Mary', 'Quant', 'ROLE_DESIGNER');
+insert into custom_user (id, username, password, first_name, last_name, role)
+values (16, 'user14', '1234', 'Elsa', 'Schiaparelli', 'ROLE_DESIGNER');
+insert into custom_user (id, username, password, first_name, last_name, role)
+values (17, 'user15', '1234', 'Kate', 'Spade', 'ROLE_DESIGNER');
+
+insert into custom_user (id, username, password, first_name, last_name, role)
+values (7, 'designer', '1234', 'Miuccia', 'Prada', 'ROLE_DESIGNER');
+insert into custom_user (id, username, password, first_name, last_name, role)
+values (8, 'vendor', '1234', 'Banana', 'Republic', 'ROLE_VENDOR');
 
 insert into order_record (order_id, user_id, num_items, total_price, purchase_date)
 values (1000, 1, 2, 98.67, '02-19-2024');
@@ -87,3 +105,5 @@ insert into order_record (order_id, user_id, num_items, total_price, purchase_da
 values (1009, 8, 1, 208.67, '02-22-2024');
 insert into order_record (order_id, user_id, num_items, total_price, purchase_date)
 values (1010, 5, 1, 208.67, '02-23-2024');
+insert into order_record (order_id, user_id, num_items, total_price, purchase_date)
+values (1011, 8, 1, 20.99, '02-23-2024');
